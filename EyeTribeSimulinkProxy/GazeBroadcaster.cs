81a1c7d6c5421c041cc5e70288f82594d14ac7cb
Paolo Burelli <burelli@gmail.com>
2015-06-09 09:45:44 +0200

    Added project with broadcaster class and UI to control the transmission of the data

---
+++ EyeTribeSimulinkProxy/GazeBroadcaster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using TETCSharpClient;
+using TETCSharpClient.Data;
+
+namespace EyeTribeSimulinkProxy
+{
+	public class GazeBroadcaster : IGazeListener
+	{
+		UdpClient client;
+		double[] data = new double[8];
+
+		public GazeBroadcaster ()
+		{
+			// Register this class for events
+			GazeManager.Instance.AddGazeListener(this);
+		}
+
+		public void OnGazeUpdate(GazeData gazeData)
+		{
+			double gX = gazeData.SmoothedCoordinates.X;
+			double gY = gazeData.SmoothedCoordinates.Y;
+
+			data [0] = gazeData.TimeStamp;
+			data [1] = gazeData.IsFixated ? 1 : 0;
+			data [2] = gazeData.SmoothedCoordinates.X;
+			data [3] = gazeData.SmoothedCoordinates.Y;
+			data [4] = gazeData.RawCoordinates.X;
+			data [5] = gazeData.RawCoordinates.Y;
+			data [6] = gazeData.LeftEye.PupilSize;
+			data [7] = gazeData.RightEye.PupilSize;
+
+			byte[] bytes = new byte[data.Length*sizeof(double)];
+			Buffer.BlockCopy (data, 0, bytes, 0, bytes.Length);
+			client.Send (bytes, bytes.Length);
+		}
+
+		public void Start(string hostname, int port) {
+			client = new UdpClient (hostname, port);
+			if (!GazeManager.Instance.IsActivated)
+				GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.Push);
+		}
+
+		public void Stop() {
+			client.Close ();
+			if (GazeManager.Instance.IsActivated)
+				GazeManager.Instance.Deactivate ();
+		}
+	}
+}
+
---
+++ EyeTribeSimulinkProxy/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Gtk;
+using EyeTribeSimulinkProxy;
+
+public partial class MainWindow: Gtk.Window
+{
+	GazeBroadcaster gb;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		gb = new GazeBroadcaster ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		gb.Stop ();
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void StartButtonPressed (object sender, EventArgs e)
+	{
+		toggleInterface ();
+		gb.Start (ipEntry.Text,int.Parse(portEntry.Text));
+	}
+
+	protected void StopButtonPressed (object sender, EventArgs e)
+	{
+		toggleInterface ();
+		gb.Stop ();
+	}
+
+	void toggleInterface() {
+		startButton.Sensitive = !startButton.Sensitive;
+		stopButton.Sensitive = !stopButton.Sensitive;
+		ipEntry.Sensitive = !ipEntry.Sensitive;
+		portEntry.Sensitive = !portEntry.Sensitive;
+	}
+}
